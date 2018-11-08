d6d0ff6d652d1df7bcd884ecef89023091782c21
etudiant <etudiant@imac-de-admin.local>
2018-11-08 14:39:37 +0400

    Initial Commit

---
+++ ProjectM1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectM1
+//
+//  Created by etudiant on 08/11/2018.
+//  Copyright © 2018 etudiant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
