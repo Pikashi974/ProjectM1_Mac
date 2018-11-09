da2aa02fda5ffbc0c341363efff1ca39ff1f14dc
etudiant <etudiant@imac-de-admin-2.local>
2018-11-09 15:59:40 +0400

    X

--- ProjectM1/PreviewViewController.swift
+++ ProjectM1/PreviewViewController.swift
@@ -1,26 +1,21 @@
 //
-//  PhotoViewController.swift
+//  PreviewViewController.swift
 //  ProjectM1
 //
-//  Created by etudiant on 08/11/2018.
+//  Created by etudiant on 09/11/2018.
 //  Copyright © 2018 etudiant. All rights reserved.
 //
 
 import UIKit
 
-class PhotoViewController: UIViewController {
+class PreviewViewController: UIViewController {
 
-    var takenPhoto: UIImage?
-    
-    @IBOutlet weak var imageView: UIImageView!
-    
-    @IBAction func cancelButton_touchUpInside(_ sender: Any) {
-    }
-    
-    @IBAction func saveButton_touchUpInside(_ sender: Any) {
+    @IBAction func cameraButton_TouchUpInside(_ sender: Any) {
+        performSegue(withIdentifier: "showView", sender: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
--- ProjectM1/ViewController.swift
+++ ProjectM1/ViewController.swift
@@ -7,16 +7,15 @@
 //
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController {
     
-    @IBOutlet weak var pickedImaged: UIImageView!
     
-
-    
+    @IBAction func takePhoto(_ sender: Any) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,49 +23,5 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Dispose of any resources that can be recreated.
     }
     
-    
-    @IBAction func camerabuttonaction(_ sender: UIButton) {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
-            imagePicker.allowsEditing = false
-            self.present(imagePicker, animated: true, completion: nil)
-        }
-        
-    }
-    
-    @IBAction func photolibraryaction(_ sender: UIButton) {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary){
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
-            imagePicker.allowsEditing = true
-            self.present(imagePicker, animated: true, completion: nil)
-        }
-        
-        
-    }
-    
-    @IBAction func saveaction(_ sender: UIButton) {
-        
-        let imageData = UIImageJPEGRepresentation(pickedImaged.image!, 0.6)
-        let compressedJPEGImage = UIImage(data: imageData!)
-        UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil)
-        saveNotice()
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!){
-        pickedImaged.image = image
-        self.dismiss(animated: true, completion: nil);
-    }
-    
-    
-    func saveNotice(){
-        let alertController = UIAlertController(title: "Image Saved!", message: "Your picture was successfully saved.", preferredStyle: .alert)
-        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alertController.addAction(defaultAction)
-        present(alertController, animated: true, completion: nil)
-    }
     
 }
