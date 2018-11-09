51740005e10a02d63301674a7c6bb1748721eac8
etudiant <etudiant@imac-de-admin-2.local>
2018-11-09 13:55:56 +0400

    Message

--- ProjectM1/ViewController.swift
+++ ProjectM1/ViewController.swift
@@ -6,134 +6,67 @@
 //  Copyright © 2018 etudiant. All rights reserved.
 //
 import UIKit
-import AVFoundation
 
-class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    let captureSession = AVCaptureSession()
-    var previewLayer:CALayer!
+    @IBOutlet weak var pickedImaged: UIImageView!
     
-    var captureDevice:AVCaptureDevice!
+
     
-    var takePhoto = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        prepareCamera()
-    }
-    
-    
-    func prepareCamera() {
-        captureSession.sessionPreset = AVCaptureSession.Preset.photo
-        
-        let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices
-            captureDevice = availableDevices.first
-            beginSession()
-        
-        
-    }
-    
-    func beginSession () {
-        do {
-            let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
-            
-            captureSession.addInput(captureDeviceInput)
-            
-        }catch {
-            print(error.localizedDescription)
-        }
-        
-        
-        self.previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            self.view.layer.addSublayer(self.previewLayer)
-            self.previewLayer.frame = self.view.layer.frame
-            captureSession.startRunning()
-            
-            let dataOutput = AVCaptureVideoDataOutput()
-            dataOutput.videoSettings = [((kCVPixelBufferPixelFormatTypeKey as NSString) as String):NSNumber(value:kCVPixelFormatType_32BGRA)]
-            
-            dataOutput.alwaysDiscardsLateVideoFrames = true
-            
-            if captureSession.canAddOutput(dataOutput) {
-                captureSession.addOutput(dataOutput)
-            }
-            
-            captureSession.commitConfiguration()
-            
-            
-            let queue = DispatchQueue(label: "com.brianadvent.captureQueue")
-            dataOutput.setSampleBufferDelegate(self, queue: queue)
-            
-            
-        
-        
-    }
-    
-    @IBAction func takePhoto(_ sender: Any) {
-        takePhoto = true
-        
-    }
-    
-    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
-        
-        if takePhoto {
-            takePhoto = false
-            
-            if let image = self.getImageFromSampleBuffer(buffer: sampleBuffer) {
-                
-                let photoVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PhotoVC") as! PhotoViewController
-                
-                photoVC.takenPhoto = image
-                
-                DispatchQueue.main.async {
-                    self.present(photoVC, animated: true, completion: {
-                        self.stopCaptureSession()
-                    })
-                    
-                }
-            }
-            
-            
-        }
-    }
-    
-    
-    func getImageFromSampleBuffer (buffer:CMSampleBuffer) -> UIImage? {
-        if let pixelBuffer = CMSampleBufferGetImageBuffer(buffer) {
-            let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
-            let context = CIContext()
-            
-            let imageRect = CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer))
-            
-            if let image = context.createCGImage(ciImage, from: imageRect) {
-                return UIImage(cgImage: image, scale: UIScreen.main.scale, orientation: .right)
-            }
-            
-        }
-        
-        return nil
-    }
-    
-    func stopCaptureSession () {
-        self.captureSession.stopRunning()
-        
-        if let inputs = captureSession.inputs as? [AVCaptureDeviceInput] {
-            for input in inputs {
-                self.captureSession.removeInput(input)
-            }
-        }
-        
-    }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    @IBAction func camerabuttonaction(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        
+    }
+    
+    @IBAction func photolibraryaction(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        
+        
+    }
+    
+    @IBAction func saveaction(_ sender: UIButton) {
+        
+        let imageData = UIImageJPEGRepresentation(pickedImaged.image!, 0.6)
+        let compressedJPEGImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil)
+        saveNotice()
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!){
+        pickedImaged.image = image
+        self.dismiss(animated: true, completion: nil);
+    }
+    
+    
+    func saveNotice(){
+        let alertController = UIAlertController(title: "Image Saved!", message: "Your picture was successfully saved.", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
     
 }
